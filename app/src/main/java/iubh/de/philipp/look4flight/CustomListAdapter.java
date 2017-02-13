63f640a97877f20acdee50ec07a8579bb4e0ee16
Philipp <stotz.philipp@googlemail.com>
2017-02-13 20:30:34 +0100

    Add Grid Layout as row

--- app/src/main/java/iubh/de/philipp/look4flight/CustomListAdapter.java
+++ app/src/main/java/iubh/de/philipp/look4flight/CustomListAdapter.java
@@ -1,6 +1,7 @@
 package iubh.de.philipp.look4flight;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -41,13 +42,22 @@ public class CustomListAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder holder;
         if (convertView == null) {
-            convertView = layoutInflater.inflate(R.layout.list_row_layout, null);
+            convertView = layoutInflater.inflate(R.layout.list_row_layout_grid, null);
             holder = new ViewHolder();
             holder.OriginView = (TextView) convertView.findViewById(R.id.list_origin);
             holder.DestinationView = (TextView) convertView.findViewById(R.id.list_destination);
             holder.DateView = (TextView) convertView.findViewById(R.id.list_date_from);
             holder.DepTimeView = (TextView) convertView.findViewById(R.id.list_dep_time);
             holder.ArrTimeView = (TextView) convertView.findViewById(R.id.list_arr_time);
+            holder.Price = (TextView) convertView.findViewById(R.id.list_price);
+
+            //holder = new ViewHolder();
+            holder.OriginView_2 = (TextView) convertView.findViewById(R.id.list_origin_2);
+            holder.DestinationView_2 = (TextView) convertView.findViewById(R.id.list_destination_2);
+            holder.DateView_2 = (TextView) convertView.findViewById(R.id.list_date_from_2);
+            holder.DepTimeView_2 = (TextView) convertView.findViewById(R.id.list_dep_time_2);
+            holder.ArrTimeView_2 = (TextView) convertView.findViewById(R.id.list_arr_time_2);
+
             convertView.setTag(holder);
         } else {
             holder = (ViewHolder) convertView.getTag();
@@ -58,6 +68,16 @@ public class CustomListAdapter extends BaseAdapter {
         holder.DateView.setText(listData.get(position).getmDate());
         holder.DepTimeView.setText(listData.get(position).getmDepTime());
         holder.ArrTimeView.setText(listData.get(position).getmArrTime());
+        holder.Price.setText(Long.toString(listData.get(position).getmPriceE()));
+
+        Log .e("Price", "Preis: " + Long.toString(listData.get(position).getmPriceE()));
+
+
+        holder.OriginView_2.setText(listData.get(position).getmCityFrom());
+        holder.DestinationView_2.setText(listData.get(position).getmCityTo());
+        holder.DateView_2.setText(listData.get(position).getmDate());
+        holder.DepTimeView_2.setText(listData.get(position).getmDepTime());
+        holder.ArrTimeView_2.setText(listData.get(position).getmArrTime());
         return convertView;
     }
 
@@ -67,6 +87,14 @@ public class CustomListAdapter extends BaseAdapter {
         TextView DateView;
         TextView DepTimeView;
         TextView ArrTimeView;
+        TextView Price;
+
+        TextView OriginView_2;
+        TextView DestinationView_2;
+        TextView DateView_2;
+        TextView DepTimeView_2;
+        TextView ArrTimeView_2;
+
     }
 
 
